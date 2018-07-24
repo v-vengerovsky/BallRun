2f63d69840a4c2ade55751b43e8a3177b5422cdd
Newton <vengerovsky_vad@mail.ru>
2018-07-24 21:41:22 +0300

    added some resources

---
+++ Assets/Scripts/GameWorld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BallRun
+{
+	public class GameWorld:MonoBehaviour
+	{
+		[SerializeField]
+		private List<BallController> BallPrefabs;
+		[SerializeField]
+		private int BallCount;
+		[SerializeField]
+		private List<GameObject> PlatformPrefabs;
+		[SerializeField]
+		private Transform StartPos;
+
+		private List<GameObject> Platforms;
+		private List<BallController> Balls;
+
+		private void Awake()
+		{
+			Platforms = new List<GameObject>();
+			Balls = new List<BallController>();
+		}
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Core;
+using Core.DI;
+
+namespace BallRun
+{
+	public class MenuController: MonoBehaviour
+	{
+		[SerializeField]
+		private Button StartButton;
+
+		private void Awake()
+		{
+			StartButton.onClick.AddListener(StartGame);
+		}
+
+		private void StartGame()
+		{
+			DependencyManager.ResolveDependency<AppStateMachine>().Fire(AppState.SceneLoading);
+		}
+
+		private void Reset()
+		{
+			ResetSerializedData();
+		}
+
+		[ContextMenu("Reset serialized data")]
+		private void ResetSerializedData()
+		{
+			StartButton = GetComponentInChildren<Button>();
+		} 
+	}
+}
